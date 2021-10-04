cc0869d2a44b9cadbf92696ba7281c67180c5f26
Ib Helmer Nielsen <ihn@ihn.dk>
2021-10-04 16:33:01 +0200

    Small error fixed

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import threading
 import tkinter
 import tkinter.scrolledtext
 from tkinter import simpledialog
+
 HOST = '127.0.0.1'
 PORT = 7913
 
@@ -19,15 +20,19 @@ class Client:
         receive_thread = threading.Thread(target=self.receive)
         gui_thread.start()
         receive_thread.start()
+
     def gui_loop(self):
         self.win =tkinter.Tk()
         self.win.configure(bg="lightgray")
+
         self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray")
         self.chat_label.config(font=("Arial",12))
         self.chat_label.pack(padx=20, pady=5)
+
         self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
         self.text_area.pack(padx=20, pady=5)
         self.text_area.config(state='disabled')
+
         self.msg_label = tkinter.Label(self.win, text="Message:", bg="lightgray")
         self.msg_label.config(font=("Arial", 12))
         self.msg_label.pack(padx=20, pady=5)
@@ -38,7 +43,9 @@ class Client:
         self.send_button=tkinter.Button(self.win, text="Send", command=self.write)
         self.send_button.config(font=("Arial",12))
         self.send_button.pack(padx=20, pady=5)
+
         self.gui_done = True
+
         self.win.protocol("WM_DELETE_WINDOW",self.stop)
         self.win.mainloop()
 
@@ -46,6 +53,7 @@ class Client:
         message = f"{self.nickname}: {self.input_area.get('1.0','end')}"
         self.sock.send(message.encode('utf-8'))
         self.input_area.delete('1.0','end')
+
     def stop(self):
         self.running = False
         self.win.destroy()
@@ -55,7 +63,7 @@ class Client:
     def receive(self):
         while self.running:
             try:
-                if message == 'NICK':
+                message = self.sock.recv(1024)
                 if message == 'NICK':
                     self.sock.send(self.nickname.encode('utf-8'))
                 else:
@@ -70,5 +78,6 @@ class Client:
                 print("Error")
                 self.sock.close()
                 break
-client = Client(HOST,PORT)
 
+client1 = Client(HOST,PORT)
+client2 = Client(HOST,PORT)
